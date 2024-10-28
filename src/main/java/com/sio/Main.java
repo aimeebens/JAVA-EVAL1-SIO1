1f1e9c8b5e3f8da49c4d1b3166622b40ff83819f
aimeebens <AimeeBENSIMON@ortmontreuil.fr>
2024-10-28 16:02:09 +0100

    Initial commit

--- src/main/java/com/sio/Main.java
+++ src/main/java/com/sio/Main.java
@@ -29,53 +29,116 @@ public class Main {
 
             switch (choice){
                 case 1:
-                    System.out.println("---------------------------------------------");
+                    System.out.println("-------------------------------------------: ");
                     System.out.println("Exercice 1");
                     System.out.println("---------------------------------------------");
-                    //Exercice 1 : Entrer votre code ici
+                    System.out.print("Entrer un nombre: ");
+                    double nombre = scanner.nextDouble();
 
+                    System.out.print("Entrer la puissance: ");
+                    int puissance = scanner.nextInt();
 
-
+                    while (puissance < 0) {
+                        System.out.println("La puissance doit être superieur ou égal à 0");
+                        System.out.print("Entrer la puissance: ");
+                        puissance = scanner.nextInt();
+                    }
+                    double resultat = Math.pow(nombre, puissance);
+                    System.out.println("le resultat de " + nombre + " puissance " + puissance + " est " + resultat);
                     System.out.println("---------------------------------------------");
                     break;
-                case 2:
+
+
+                    case 2:
                     System.out.println("---------------------------------------------");
                     System.out.println("Exercice 2");
                     System.out.println("---------------------------------------------");
-                    //Exercice 2 : Entrer votre code ici
+                    DecimalFormat df = new DecimalFormat("##.###");
+                    System.out.print("Entrer le nombre de kilomètre parcourus par le client: ");
+                    double nombreKm = scanner.nextDouble();
+                    double montant;
 
+                    while (nombreKm < 1){
+                        System.out.println("Le nombre de kilomètre doit superieur ou égal à 1");
+                        System.out.print("Entrer le nombre de kilomètre parcourus par le client: ");
+                        nombreKm = scanner.nextDouble();
+                    }
 
-
-
-                    System.out.println("---------------------------------------------");
+                    if (nombreKm < 100){
+                        montant = nombreKm * 0.20;
+                    }
+                    else if (nombreKm < 500){
+                        montant = nombreKm * 0.50;
+                    }
+                    else {
+                        montant = nombreKm * 0.80;
+                    }
+                    System.out.print("Le montant à payer par le client est: " + df.format(montant));
+                    System.out.println("\n---------------------------------------------");
                     break;
-                case 3:
+
+
+
+
+                    case 3:
                     System.out.println("---------------------------------------------");
                     System.out.println("Exercice 3");
                     System.out.println("---------------------------------------------");
-                    //Exercice 3 : Entrer votre code ici
+                    System.out.println("Entrer la largeur du rectangle: ");
+                    double largeur = scanner.nextDouble();
 
+                    while (largeur < 1){
+                        System.out.println("la largeur doit être superieur ou égal à 1");
+                        System.out.print("Entrer la largeur du rectangle: ");
+                        largeur = scanner.nextDouble();
+                    }
+                    System.out.println("Entrer la hauteur du rectangle: ");
+                    double hauteur = scanner.nextDouble();
 
-
-
+                    while (hauteur < 1) {
+                        System.out.println("la hauteur doit être superieur ou égal à 1");
+                        System.out.println("Entrer la hauteur du rectangle: ");
+                        hauteur = scanner.nextDouble();
+                    }
+                    for (int i = 0; i < hauteur; i++){
+                        for (int j = 0; j < largeur; j++){
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
                     System.out.println("---------------------------------------------");
-
                     break;
-                case 4:
+
+
+
+                    case 4:
                     System.out.println("---------------------------------------------");
                     System.out.println("Exercice 4");
                     System.out.println("---------------------------------------------");
-                    //Exercice 4 : Entrer votre code ici
 
+                    System.out.println("Entrer un nombre :");
+                    nombre = scanner.nextDouble();
 
-
-
+                    while (nombre < 1){
+                        System.out.println("le nombre doit être superieur ou égal à 1");
+                        System.out.print("Entrer un nombre :");
+                        nombre = scanner.nextDouble();
+                    }
+                    long factorielle = 1;
+                    int entier = (int) nombre;
+                    for (int i = 1; i <= entier; i++) {
+                        factorielle *= i;
+                    }
+                    System.out.println("le factoriel du nombre est : " + factorielle);
                     System.out.println("---------------------------------------------");
                     break;
                 case 0:
                     System.out.println(RESET);
                     System.exit(0);
                     break;
+
+                default: System.out.println("Choix invalide"); break;
+
             }
         }
     }
